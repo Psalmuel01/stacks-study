643a4dff04d33eacf51ae37aee5abc3c067a5971
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2025-09-01 16:26:52 +0100

    uodate contracts

--- secret-wisher-dapp/vite.config.ts
+++ secret-wisher-dapp/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+   server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     tailwindcss(),
   ],
